ed32c60ab9381b7a8c44f3993fe85738672b20a2
Repro12 <111174041+Repro12@users.noreply.github.com>
2022-08-14 05:06:25 -0700

    Add files via upload

---
+++ Corstyle Sports.js
@@ -0,0 +1,7 @@
+function amazon(){
+    window.location.href= "https://www.amazon.in/Corstyle-Outdoors-Sneakers-Training-White-Black/dp/B09ZDDVKVK/ref=sr_1_1?crid=22O4A30OULUBJ&keywords=Corstyle+Sports&qid=1660034096&sprefix=corstyle+sports%2Caps%2C550&sr=8-1";
+};
+
+function flipkart(){
+    window.location.href= "https://www.flipkart.com/corstyle-trending-stylish-high-quality-material-aimix-sole-light-weighted-mesh-running-shoes-men/p/itmfbd847f4ef94f?pid=SHOGAD3W4PVSZEKA&lid=LSTSHOGAD3W4PVSZEKA5XHLDN&marketplace=FLIPKART&q=Corstyle+Sports&store=search.flipkart.com&srno=s_1_4&otracker=search&otracker1=search&fm=organic&iid=71949f3c-35d5-49da-9e6f-371b28237f60.SHOGAD3W4PVSZEKA.SEARCH&ppt=pp&ppn=pp&ssid=xz0a9c6a9c0000001660034239008&qH=0d3110a0149b205b";
+};
